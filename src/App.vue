dcb992fa1a64b8d19040500695a68e88d1a1be3c
Adif <adifiajuniansyah@gmail.com>
2025-04-30 16:26:56 +0700

    Dapat melihat kegiatan yang sudah ditambahkan di aplikasi

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,87 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue'
+
+// State daftar kegiatan
+const activities = ref([
+  'Belajar Vue.js',
+  'Mengerjakan tugas kuliah',
+  'Olahraga sore',
+  'Membaca buku'
+])
+
+// State untuk input kegiatan baru
+const newActivity = ref('')
+
+// Fungsi untuk menambahkan kegiatan baru
+const addActivity = () => {
+  if (newActivity.value.trim() !== '') {
+    activities.value.push(newActivity.value)
+    newActivity.value = ''
+  }
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="container">
+    <h1>Daftar Kegiatan</h1>
+
+    <!-- Form Tambah Kegiatan -->
+    <div class="form">
+      <input
+        type="text"
+        v-model="newActivity"
+        placeholder="Masukkan kegiatan baru"
+        @keyup.enter="addActivity"
+      />
+      <button @click="addActivity">Tambah</button>
+    </div>
+
+    <!-- Daftar Kegiatan -->
+    <ul>
+      <li v-for="(activity, index) in activities" :key="index">
+        {{ activity }}
+      </li>
+    </ul>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+.container {
+  max-width: 600px;
+  margin: auto;
+  padding: 2em;
+  font-family: Arial, sans-serif;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+
+h1 {
+  text-align: center;
+  margin-bottom: 1em;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+.form {
+  display: flex;
+  gap: 0.5em;
+  margin-bottom: 1.5em;
+}
+
+input {
+  flex: 1;
+  padding: 0.5em;
+  font-size: 1em;
+}
+
+button {
+  padding: 0.5em 1em;
+  font-size: 1em;
+  cursor: pointer;
+}
+
+ul {
+  padding-left: 1.2em;
+}
+
+li {
+  margin: 0.5em 0;
+  list-style: disc;
 }
 </style>
